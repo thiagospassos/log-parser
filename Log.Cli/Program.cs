5b295396afedd5832808461e38c54c2f4c5b9bfa
Thiago Passos <thiago@passos.com.au>
2024-08-08 12:42:02 +1000

    Updating readme file and creating CLI

---
+++ Log.Cli/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Log.Application;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var filePath = "programming-task-example-data.log";
+        if (args.Any())
+            filePath = args[0];
+
+        ILogService service = new LogService();
+
+        try
+        {
+            using (var stream = File.OpenRead(filePath))
+            {
+                service.ProcessLogFile(stream);
+
+                PrintColored("Number of log entries: ", ConsoleColor.DarkGreen);
+                PrintColoredLine($"{service.NumberOfEntries()}\n", ConsoleColor.White);
+                
+                
+                PrintColored("Number of unique IP addresses: ", ConsoleColor.DarkGreen);
+                PrintColoredLine($"{service.NumberOfUniqueVisitors()}\n", ConsoleColor.White);
+
+                var topUrls = service.TopVisitedUrls();
+                PrintColoredLine("Top Visited URLs: ", ConsoleColor.DarkGreen);
+                foreach (var topUrl in topUrls)
+                {
+                    PrintColoredLine(topUrl, ConsoleColor.White);
+                }
+
+                var topVisitors = service.TopActiveVisitors();
+                PrintColoredLine("\nTop Visitors: ", ConsoleColor.DarkGreen);
+                foreach (var topVisitor in topVisitors)
+                {
+                    PrintColoredLine(topVisitor, ConsoleColor.White);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            PrintColored("Failure: ", ConsoleColor.DarkRed);
+            PrintColoredLine(ex.Message, ConsoleColor.White);
+        }
+    }
+
+    private static void PrintColored(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.Write(text);
+    }
+
+    private static void PrintColoredLine(string text, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(text);
+        Console.ResetColor();
+    }
+}
--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 This goal of this application is to read a log file and be able to analyse its data based on the sample data was provided.
 
-# Log Example and assumptions
+# Log Sample
 
 50.112.00.11 - admin [11/Jul/2018:17:33:01 +0200] "GET /asset.css HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6"
+
+# Assumptions
 
 * The data will be broken down by white space while considering quotes and brackets as text qualifiers
 * In the example above, here are my assumptions regarding each field:
@@ -19,3 +21,19 @@ This goal of this application is to read a log file and be able to analyse its d
   * User Agent: "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6"
 * When there are multiple URL with the same number of visits, the timestamp will be used for ranking
 * When there are IP Addresses with the same number of visits, the timestamp will be used for ranking
+
+# Testing
+
+* in the console, navigate to the `Log.Tests` folder
+* for a simple output, run `dotnet test`
+* if you want to list all tests, you may run `dotnet test --logger "console;verbosity=detailed"`
+
+![output](2024-08-08_12-38-11.jpg)
+
+# Running Cli
+
+* in the console, navigate to the `Log.Cli` folder
+* run `dotnet run`
+
+
+![output](2024-08-08_12-28-22.jpg)
